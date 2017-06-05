32651cef54a90cc4e235d577bf949b2925f95772
thenry0401 <thenry0401@gmail.com>
2017-06-05 12:51:47 +0900

    Person모델 수정(choices, recursive, foreign key 추가), Car, Manufacturer 모델 추가

--- django_app/documentation/settings.py
+++ django_app/documentation/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p$vw$#noxj42!^5uvk)sbhmf(t3dl*vgkvmds(%zu72!r$i71&'
+SECRET_KEY = 'q@m&29i%-apuog&8&*yn9!p_!mxc+4pu(yg-n)d=)o&^6+nvdu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+
+    'introduction_to_models',
 ]
 
 MIDDLEWARE = [
