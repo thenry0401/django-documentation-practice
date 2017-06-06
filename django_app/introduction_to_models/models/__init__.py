dbc4114dc64c5ec2bf908e91e7667df8210f0083
thenry0401 <thenry0401@gmail.com>
2017-06-07 00:19:25 +0900

    intermediate 실습 모델 추가(Player, Club, TradeInfo)

--- django_app/introduction_to_models/models/__init__.py
+++ django_app/introduction_to_models/models/__init__.py
@@ -2,3 +2,4 @@ from .car import Car, Manufacturer
 from .person import Person
 from .many_to_one_example import Reporter, Article
 from .topping_and_pizza import Topping, Pizza
+from .intermediate import Player, Club, TradeInfo
